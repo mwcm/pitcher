b86ab79e8d768720480d90e30d853f325ec8780e
morgan <mwcmitchell@gmail.com>
2020-05-28 14:47:29 -0400

    research & start of a/b testing

--- pitcher.py
+++ pitcher.py
@@ -16,8 +16,30 @@ ST_NEGATIVE = {-1: 1.05652677103003,
 INPUT_SAMPLE_RATE = 44100
 TARGET_SAMPLE_RATE = 26040
 
+# TODO
+# http://www.synthark.org/Archive/EmulatorArchive/SP1200.html
+# The sample input goes via an anti-aliasing filter to remove unwanted frequencies that are above half the sample frequency, the cutoff is brick walled at 42dB.
+
+
+# TODO
+# https://ccrma.stanford.edu/~dtyeh/papers/yeh07_icmc_sp12.pdf
+# To simulate aliasing accurately using a digital implemen-tation,  the  discrete-time  signal  is  ideally  interpolated  tothe  time-grid  corresponding  to  the  sampling  rate  of  theSP-12.Methods to approximate the ideal interpolation includeusing a variable delay filter [2], or resampling[5] to a mul-tiple of the SP-12’s sampling rate and then downsamplingto the SP-12 rate.
+
+
 # TODO: pitching up works, but 0 st outputs lower pitched file
 # TODO: librosa resamples on load, what was the JS behaviour?
+
+
+
+def manual_pitch(y):
+
+def auto_pitch(y)
+    pitched = librosa.effects.pitch_shift(y, TARGET_SAMPLE_RATE, n_steps=st)
+    return pitched
+
+def time_shift(y):
+    pitched = librosa.effects.time_stretch(y, TARGET_SAMPLE_RATE, n_steps=st)
+    return pitched
 
 
 @click.command()
@@ -35,7 +57,6 @@ def pitch(file, st):
 
     y = librosa.core.resample(y, INPUT_SAMPLE_RATE, TARGET_SAMPLE_RATE)
 
-    new = librosa.effects.pitch_shift(y, TARGET_SAMPLE_RATE, n_steps=st)
     # n = int(np.round(len(y) * t))
     # r = np.linspace(0, len(y), n)
     # new = np.zeros(n, dtype=np.float32)
