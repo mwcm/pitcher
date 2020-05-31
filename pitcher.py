f9e41a49739f8d834fe18a6b5713d2e4ff50b171
morgan <mwcmitchell@gmail.com>
2020-05-30 20:22:23 -0400

    testing

--- pitcher.py
+++ pitcher.py
@@ -23,6 +23,8 @@ DELTA_S = 2 * U/QUANTIZATION_LEVELS  # level distance
 
 S_MIDRISE = -U + DELTA_S/2 + np.arange(QUANTIZATION_LEVELS)*DELTA_S
 S_MIDTREAD = -U + np.arange(QUANTIZATION_LEVELS)*DELTA_S
+S_MIDRISE = S_MIDRISE.astype(np.float32)
+S_MIDTREAD = S_MIDTREAD.astype(np.float32)
 
 INPUT_SAMPLE_RATE = 96000
 OUTPUT_SAMPLE_RATE = 48000
@@ -48,6 +50,15 @@ RESAMPLE_METHODS = ['librosa', 'scipy']
 # - resample to multiple of sp 1200 rate
 # - resample to sp 1200 rate
 # - resample to 48khz for output
+
+
+# https://stackoverflow.com/questions/1094841/reusable-library-to-get-human-readable-version-of-file-size
+def sizeof_fmt(num, suffix='B'):
+    for unit in ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi']:
+        if abs(num) < 1024.0:
+            return "%3.1f%s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f%s%s" % (num, 'Yi', suffix)
 
 
 # TODO: allow for lower than -8 st
@@ -119,9 +130,12 @@ def quantize(x, S):
     S = S.reshape((1, -1))
     dists = abs(X-S)
 
+    print(S.dtype)
+    print(sizeof_fmt(dists.nbytes))
+
     nearestIndex = dists.argmin(axis=1)
     quantized = S.flat[nearestIndex]
-    return quantized
+    return quantized.reshape(x.shape)
 
 
 # NOTE: maybe skip the anti aliasing?
