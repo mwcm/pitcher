c802ce99586db9ecbeafaf24a433867d923b2684
morgan <mwcmitchell@gmail.com>
2020-06-17 16:08:39 -0400

    further improve quantize speed with cdktree

--- pitcher.py
+++ pitcher.py
@@ -12,9 +12,6 @@ from numba import jit
 from pyrubberband import pyrb
 
 POSITIVE_TUNING_RATIO = 1.02930223664
-# TODO: maybe revisit, "sp-12 has 32 different tuning settings with various skip amounts"
-# according to paper looks like skip of ~0.64062 creates nice aliasing?
-# these all look like a skip of ~ 0.06
 NEGATIVE_TUNING_RATIOS = {-1: 1.05652677103003,
                           -2: 1.1215356033380033,
                           -3: 1.1834835840896631,
@@ -24,24 +21,22 @@ NEGATIVE_TUNING_RATIOS = {-1: 1.05652677103003,
                           -7: 1.5028019735639886,
                           -8: 1.5766735700797954}
 
+# https://dspillustrations.com/pages/posts/misc/quantization-and-quantization-noise.html
+U = 1  # max amplitude to quantize
 QUANTIZATION_BITS = 12
 QUANTIZATION_LEVELS = 2 ** QUANTIZATION_BITS
-U = 1  # max Amplitude to be quantized TODO: revisit?
 DELTA_S = 2 * U / QUANTIZATION_LEVELS  # level distance
-
 S_MIDRISE = -U + DELTA_S / 2 + np.arange(QUANTIZATION_LEVELS) * DELTA_S
 S_MIDTREAD = -U + np.arange(QUANTIZATION_LEVELS) * DELTA_S
 
 RESAMPLE_MULTIPLIER = 2
-ZERO_ORDER_HOLD_MULTIPLIER = 4
+ZOH_MULTIPLIER = 4
 
-INPUT_SAMPLE_RATE = 96000
-OUTPUT_SAMPLE_RATE = 48000
-TARGET_SAMPLE_RATE = 26040
+INPUT_SR = 96000
+OUTPUT_SR = 48000
+TARGET_SR = 26040
+TARGET_SR_MULTIPLE = TARGET_SR * RESAMPLE_MULTIPLIER
 
-TARGET_SAMPLE_RATE_MULTIPLE = TARGET_SAMPLE_RATE * RESAMPLE_MULTIPLIER
-
-# map these if they grow any longer
 PITCH_METHODS = ['manual', 'rubberband']
 RESAMPLE_METHODS = ['librosa', 'scipy']
 
@@ -53,14 +48,13 @@ def manual_pitch(x, st):
         t = POSITIVE_TUNING_RATIO ** -st
     elif st == 0:  # no change
         return x
-    else:  # -8 > st: extrapolate, loses a few points of percision for some reason
+    else:  # -8 > st: extrapolate, seems to lose a few points of percision?
         f = sp.interpolate.interp1d(list(NEGATIVE_TUNING_RATIOS.keys()),
                                     list(NEGATIVE_TUNING_RATIOS.values()),
                                     fill_value='extrapolate')
         t = f(st)
 
     n = int(np.round(len(x) * t))
-    # - 1 accounts for rounding issues
     r = np.linspace(0, len(x) - 1, n).round().astype(np.int32)
     pitched = [x[r[e]] for e in range(n-1)]  # could yield here
     return pitched
@@ -69,18 +63,17 @@ def manual_pitch(x, st):
 def filter_input(x):
     # approximating the anti aliasing filter, don't think this needs to be
     # perfect since at fs/2=13.02kHz only -10dB attenuation, might be able to
-    # improve accuracy in the 15 -> 20kHz range with firwin
+    # improve accuracy in the 15 -> 20kHz range with firwin?
     f = sp.signal.ellip(4, 1, 72, 0.666, analog=False, output='sos')
     y = sp.signal.sosfilt(f, x)
     return y
 
 
 def filter_output(x):
-    # use window method to replicate the fixed output filter
     freq = np.array([0, 6510, 8000, 10000, 11111, 13020, 15000, 17500, 20000, 24000])
     att = np.array([0, 0, -5, -10, -15, -23, -28, -35, -41, -40])
     gain = np.power(10, att/20)
-    f = sp.signal.firwin2(45, freq, gain, fs=OUTPUT_SAMPLE_RATE, antisymmetric=False)
+    f = sp.signal.firwin2(45, freq, gain, fs=OUTPUT_SR, antisymmetric=False)
     sos = sp.signal.tf2sos(f, [1.0])
     y = sp.signal.sosfilt(sos, x)
     return y
@@ -88,22 +81,19 @@ def filter_output(x):
 
 def pyrb_pitch(y, st):
     t = POSITIVE_TUNING_RATIO ** st  # revisit when replacing pyrb
-    pitched = pyrb.pitch_shift(y, TARGET_SAMPLE_RATE, n_steps=st)
+    pitched = pyrb.pitch_shift(y, TARGET_SR, n_steps=st)
     return time_stretch(pitched, t)
 
 
 def librosa_resample(y):
-    # http://www.synthark.org/Archive/EmulatorArchive/SP1200.html
-    # "...resample to a multiple of the SP-12(00)'s sampling rate..."
-    resampled = resample(y, INPUT_SAMPLE_RATE, TARGET_SAMPLE_RATE_MULTIPLE)
-    # "...then downsample to the SP-12(00) rate"
-    downsampled = resample(resampled, TARGET_SAMPLE_RATE_MULTIPLE, TARGET_SAMPLE_RATE)
+    resampled = resample(y, INPUT_SR, TARGET_SR_MULTIPLE)
+    downsampled = resample(resampled, TARGET_SR_MULTIPLE, TARGET_SR)
     return downsampled
 
 
 def scipy_resample(y):
-    seconds = len(y)/INPUT_SAMPLE_RATE
-    target_samples = int(seconds * TARGET_SAMPLE_RATE_MULTIPLE) + 1
+    seconds = len(y)/INPUT_SR
+    target_samples = int(seconds * TARGET_SR_MULTIPLE) + 1
     resampled = sp.signal.resample(y, target_samples)
     decimated = sp.signal.decimate(resampled, RESAMPLE_MULTIPLIER)
     return decimated
@@ -112,53 +102,49 @@ def scipy_resample(y):
 def zero_order_hold(y):
     # intentionally oversample by repeating each sample 4 times
     # could also try a freq aliased sinc filter
-    return np.repeat(y, ZERO_ORDER_HOLD_MULTIPLIER)
+    return np.repeat(y, ZOH_MULTIPLIER)
 
 
+# TODO: revisit
 # we'd like output to be measured in the same units as x, just quantized
 # rescaling currently isn't working, maybe revisit this
 # peaks also seem slightly lower using this vs original quantize method
 def sar_quantize(x):
     ncomp = 0.001  # noise of the comparator
-    ndac = 0  # noise of the C-DAC
-    nsamp = 0  # sampling kT/C noise
+    ndac = 0       # noise of the C-DAC
+    nsamp = 0      # sampling kT/C noise
 
     myadc = SAR(QUANTIZATION_BITS, ncomp, ndac, nsamp, 2)
     normalized, center, maxbin = normalize_input(x)
-    print(normalized)
     # run adc
     adcout = myadc.sarloop(normalized)
     _ = ''  # throwaway, don't need center or maxbin here
     adcout, _, _, normalize_input(adcout)
     # rescale to original
     rescaled = adcout * maxbin + center
-    print(rescaled)
     return rescaled
 
 
-# interp1d type='nearest' might be a faster way
+def nearest_values(x, y):
+    x, y = map(np.asarray, (x, y))
+    tree = sp.spatial.cKDTree(y[:, None])
+    ordered_neighbors = tree.query(x[:, None], 1)[1]
+    return ordered_neighbors
+
+
+# no audible difference after audacity invert test @ 12 bits
+# however, when plotted the scaled amplitude of quantized audio is
+# noticeably higher than the original
 def quantize(x, S):
-
-    X = x.reshape((-1, 1)).astype(np.float32)
-    S = S.reshape((1, -1)).astype(np.float32)  # don't think this is necessary
-
-    @jit(nopython=True)
-    def nearest_value(X, S):
-        y = np.zeros(len(X), dtype=np.int32)
-        for i, item in enumerate(X):
-            y[i] = np.abs(item-S).argmin()
-        return y
-
-    y = []
-    X = np.asfortranarray(X)
+    x = np.asfortranarray(x)
     S = np.asfortranarray(S)
-    y = nearest_value(X, S)
+    y = nearest_values(x, S)
     quantized = S.flat[y].reshape(x.shape)
     return quantized
 
 
-# similar to sar, output of digitize would need to be rescaled back to original
-# we'd like output to be measured in the same units as x, just quantized
+# same issue as SAR, output needs to be rescaled
+# we'd like output to be the same scale as input, just quantized
 def digitize(x, S):
     y = np.digitize(x.flatten(), S.flatten())
     return y
@@ -167,12 +153,11 @@ def digitize(x, S):
 # TODO
 # - requirements
 # - readme
-# - impletement vcf? (ring moog)
-# - improve input filter fit
 # - logging
-# - revisit pitch values
+# - impletement optional vcf? (ring moog) good description in slides
+# - improve input filter fit
 # - replace or delete pyrb
-# - replace librosa if there is a module with better performance
+# - replace librosa if there is a module with better performance, maybe essentia?
 # - supress librosa numba warning
 
 # NOTES:
@@ -198,17 +183,14 @@ def pitch(file, st, pitch_method, resample_method, output_file,
           skip_input_filter, skip_output_filter, skip_quantize,
           normalize):
 
-    # oversample to 96khz
-    y, s = load(file, sr=INPUT_SAMPLE_RATE)
+    y, s = load(file, sr=INPUT_SR)
 
     if not skip_input_filter:
-        # anti aliasing filter
         y = filter_input(y)
 
     if resample_method in RESAMPLE_METHODS:
-        # resample to target sample rate
         if resample_method == RESAMPLE_METHODS[0]:
-            resampled = librosa_resample(y)
+            resampled = librosa_resample(y)  # should specify SR's here not in function
         elif resample_method == RESAMPLE_METHODS[1]:
             resampled = scipy_resample(y)
     else:
@@ -229,18 +211,17 @@ def pitch(file, st, pitch_method, resample_method, output_file,
                          f'valid methods are {PITCH_METHODS}')
 
     # oversample again (default factor of 4)
+    # TODO: retest output, test freq aliased sinc fn
     post_zero_order_hold = zero_order_hold(pitched)
 
-    # resample for output filter
-    # TODO investigate the exception that arises when fortranarray cast is rm'd
+    # give option use scipy resample here?
     output = resample(np.asfortranarray(post_zero_order_hold),
-                      TARGET_SAMPLE_RATE * ZERO_ORDER_HOLD_MULTIPLIER, OUTPUT_SAMPLE_RATE)
+                      TARGET_SR * ZOH_MULTIPLIER, OUTPUT_SR)
 
     if not skip_output_filter:
-        # low pass equalization filter
-        output = filter_output(output)
+        output = filter_output(output)  # equalization filter
 
-    af.write(output_file, output, OUTPUT_SAMPLE_RATE, '16bit', normalize)
+    af.write(output_file, output, OUTPUT_SR, '16bit', normalize)
 
 
 if __name__ == '__main__':
